0e8fb72c678b82cc68ca550078c6183008310e50
LeeJiHyeon <rachel2148072@gmail.com>
2023-04-13 16:09:45 +0900

    fix:create-account 페이지 이동시 login페이지로 가는 middleware오류 수정

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(req: NextRequest) {
+  if (req.url.includes("create-account")) return;
   if (!req.cookies.has("mini-twitter") && !req.url.includes("log-in")) {
     return NextResponse.redirect(new URL("/log-in", req.url));
   }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,10 @@ import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import useMutation from "@/lib/client/useMutation";
 import useUser from "@/lib/client/useUser";
-import useSWR from "swr";
+import useSWR, { SWRConfig } from "swr";
 import Link from "next/link";
 import { Tweet, User } from "@prisma/client";
+import { NextPage } from "next";
 
 interface TweetWithCount extends Tweet {
   _count: {
@@ -18,13 +19,11 @@ interface TweetRes {
   tweets: TweetWithCount[];
 }
 
-export default function Home() {
-  const { user, isLoading } = useUser();
+const Home: NextPage = () => {
   const { register, handleSubmit } = useForm<Tweet>();
-  const [mutation, { data: postingTweet, loading, error }] =
-    useMutation("/api/tweet");
-  const { data: tweetsRes, mutate } = useSWR<TweetRes>("/api/tweet");
-
+  const [mutation, { data: postingTweet, loading }] =
+    useMutation("/api/tweets");
+  const { data: tweetsRes, mutate } = useSWR<TweetRes>("/api/tweets");
   const onValid = (tweetData: Tweet) => {
     if (loading) return;
     mutation({ data: tweetData, method: "POST" });
@@ -32,25 +31,48 @@ export default function Home() {
   useEffect(() => {
     mutate();
   }, [postingTweet]);
-  console.log(tweetsRes);
   return (
     <>
       <h1>HOME</h1>
-      <h2>{user?.name}</h2>
+      {/* <h2>{user?.name}</h2> */}
       <form onSubmit={handleSubmit(onValid)}>
         <input {...register("text")} placeholder="트윗을 작성해 주세요." />
         <button>트윗하기</button>
       </form>
       <div>
-        {tweetsRes?.tweets?.map(tweet => (
-          <div key={tweet.id}>
-            <span>{tweet.user.name}</span>
-            <Link href={`/tweet/${tweet.id}`}>| {tweet.text}</Link>
-            <span>| 코멘트 : {tweet._count.comments}</span>
-            <span>| 좋아요 : {tweet._count.likes}</span>
-          </div>
-        ))}
+        {tweetsRes
+          ? tweetsRes?.tweets?.map(tweet => (
+              <div key={tweet.id}>
+                <span>{tweet.user.name}</span>
+                <Link href={`/tweets/${tweet.id}`}>| {tweet.text}</Link>
+                <span>| 코멘트 : {tweet._count.comments}</span>
+                <span>| 좋아요 : {tweet._count?.likes || 0}</span>
+              </div>
+            ))
+          : "Loading.."}
       </div>
     </>
   );
+};
+
+const Page: NextPage<{ tweets: TweetWithCount[] }> = ({ tweets }) => {
+  return (
+    <SWRConfig
+      value={{
+        fallback: { "/api/tweets": { success: true, tweets } },
+      }}>
+      <Home />
+    </SWRConfig>
+  );
+};
+
+export async function getServerSideProbs() {
+  const tweets = await db?.tweet.findMany({});
+  return {
+    props: {
+      tweets: JSON.parse(JSON.stringify(tweets)),
+    },
+  };
 }
+
+export default Page;
